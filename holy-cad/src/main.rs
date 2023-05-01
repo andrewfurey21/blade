78ba5e5c113a661475a63ba8b9d1828d1268b981
andrewfurey21 <andrewfurey2003@gmail.com>
2023-05-01 22:20:18 +0100

    playing with ash/vulkan

--- holy-cad/src/main.rs
+++ holy-cad/src/main.rs
@@ -1,19 +1,21 @@
 
-#![allow(unused)]
+//#![allow(unused)]
 use anyhow::Result;
 use ash::{self, vk};
 
 
 fn main() -> Result<()> {
-    let entry = unsafe { ash::Entry::load() }?;
+    let entry = unsafe {ash::Entry::load()?};
     let app_info = vk::ApplicationInfo {
-        api_version: vk::make_api_version(0, 1, 0, 0),
+        api_version: vk::API_VERSION_1_3,
         ..Default::default()
     };
     let create_info = vk::InstanceCreateInfo {
         p_application_info: &app_info,
         ..Default::default()
     };
-    let instance = unsafe { entry.create_instance(&create_info, None) }?;
+    let instance = unsafe { entry.create_instance(&create_info, None)? };
+
     return Ok(());
 }
+
