d2ced55461e65af947e9a09b40f27b9055097511
andrewfurey21 <andrewfurey2003@gmail.com>
2023-05-01 18:59:49 +0100

    started using ash, will use it for vulkan

--- holy-cad/src/main.rs
+++ holy-cad/src/main.rs
@@ -1,6 +1,19 @@
 
-use holy_cad::run;
+#![allow(unused)]
+use anyhow::Result;
+use ash::{self, vk};
 
-fn main() {
-    run();
+
+fn main() -> Result<()> {
+    let entry = unsafe { ash::Entry::load() }?;
+    let app_info = vk::ApplicationInfo {
+        api_version: vk::make_api_version(0, 1, 0, 0),
+        ..Default::default()
+    };
+    let create_info = vk::InstanceCreateInfo {
+        p_application_info: &app_info,
+        ..Default::default()
+    };
+    let instance = unsafe { entry.create_instance(&create_info, None) }?;
+    return Ok(());
 }
