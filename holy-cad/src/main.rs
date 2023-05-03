0a84bb467b8db7f13ce55618fe5f59de633977d9
andrewfurey21 <andrewfurey2003@gmail.com>
2023-05-03 23:14:15 +0100

    added some gpu allocation

--- holy-cad/src/main.rs
+++ holy-cad/src/main.rs
@@ -1,6 +1,10 @@
 #![allow(unused)]
 use anyhow::{Context, Result};
 use ash::{self, vk};
+use gpu_allocator::vulkan::*;
+use gpu_allocator::MemoryLocation;
+
+
 
 fn main() -> Result<()> {
     let entry = unsafe { ash::Entry::load() }?;
@@ -34,8 +38,8 @@ fn main() -> Result<()> {
         p_queue_create_infos: &queue_create_infos[0],
         ..Default::default()
     };
-
     let device = unsafe { instance.create_device(physical_device, &create_info, None)? };
+
     let queue = unsafe { device.get_device_queue(0, 0) };
 
     let command_pool = {
@@ -55,10 +59,54 @@ fn main() -> Result<()> {
             .context("No command buffer found.")?
     };
 
+    let value_count = 16;
+    let value = 314;
+
+   let buffer = {
+       let buffer_create_info = vk::BufferCreateInfo {
+           size: value_count*std::mem::size_of::<i32>() as vk::DeviceSize,
+           usage: vk::BufferUsageFlags::UNIFORM_BUFFER,
+           ..Default::default()
+       };
+
+      unsafe { device.create_buffer(&buffer_create_info, None) }?
+   };
+   let mut allocator = {
+       let allocator_create_description = AllocatorCreateDesc {
+            instance: instance.clone(),
+            device: device.clone(),
+            physical_device: physical_device.clone(),
+            debug_settings: Default::default(),
+            buffer_device_address: true,
+       };
+       Allocator::new(&allocator_create_description).unwrap()
+
+    };
+
+    let allocation = {
+
+        let memory_requirements = unsafe { device.get_buffer_memory_requirements(buffer) };
+
+        let allocation_create_desc = AllocationCreateDesc {
+            name: "Buffer Allocation",
+            requirements: memory_requirements,
+            location: MemoryLocation::GpuToCpu,
+            linear: true,
+            allocation_scheme: AllocationScheme::GpuAllocatorManaged,
+        };
+
+        let allocation = allocator.allocate(&allocation_create_desc).unwrap();
+        unsafe { device.bind_buffer_memory(buffer, allocation.memory(), allocation.offset()); }
+        allocation
+    };
+
+    allocator.free(allocation).unwrap();
 
     unsafe {
+        device.destroy_buffer(buffer, None);
         device.destroy_device(None);
         instance.destroy_instance(None);
     }
     return Ok(());
 }
+
