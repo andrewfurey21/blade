5288ae19db1b0340d18343afcd0bb476e5ecf137
andrewfurey21 <andrewfurey2003@gmail.com>
2023-05-28 20:05:13 +0100

    added messages and changed to cargo run

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
+echo "Building vertex shader"
 glslc shaders/shader.vert -o shaders/vert.spv
+echo "Building fragment shader"
 glslc shaders/shader.frag -o shaders/frag.spv
 cd cad-rs
-cargo build
+echo "Building project"
+cargo run
