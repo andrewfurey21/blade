5b58f69994ed212be9ddd9ee36923323f79d18b5
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-06 21:41:28 +0100

    added some extra commands

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@ glslc shaders/src/shader.vert -o shaders/spv/vert.spv
 echo "Building fragment shader"
 glslc shaders/src/shader.frag -o shaders/spv/frag.spv
 echo "Building project"
-cargo fmt | cargo run
+cargo fmt
+# cargo clippy
+# cargo audit
+cargo run
