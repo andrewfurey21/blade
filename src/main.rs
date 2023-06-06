9049004a41b7f30a68832c80eced4f21ef6e5bfa
andrewfurey21 <andrewfurey2003@gmail.com>
2023-06-06 21:42:34 +0100

    cleaned up main, instantiated vulkan app

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
+use crate::app::App;
+use log::{error, info};
+
+mod app;
+mod constants;
 
 fn main() {
     env_logger::init();
 
     info!("Starting up application...");
-
-    let output = run();
-    if let Err(error_description) = output {
-        error!("Error: {}", error_description);
+    let app = App::new();
+    if let Err(desc) = app {
+        error!("{}", desc);
     }
+
+    // let run = app.run();
+    // if let Err(desc) = run {
+    //  error!("{}", desc);
+    //}
+    //
     info!("Exiting application.");
 }
