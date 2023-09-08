3b9b24ab75851c15aefdd3882cd7919cab206373
andrewfurey21 <andrewfurey2003@gmail.com>
2023-09-08 11:01:53 +0100

    removed messages

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 
 # add check for spv folder
-echo "Building vertex shader"
 glslc shaders/src/shader.vert -o shaders/spv/vert.spv
-echo "Building fragment shader"
 glslc shaders/src/shader.frag -o shaders/spv/frag.spv
-echo "Building project"
 cargo fmt
-# cargo clippy
 cargo run
