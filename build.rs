acce814159b086628e46fba27b557167a4b6b59b
andrewfurey21 <andrewfurey2003@gmail.com>
2023-11-22 20:41:35 +0000

    reducing nested statements

--- build.rs
+++ build.rs
@@ -28,56 +28,56 @@ fn main() {
         fs::create_dir(output_dir).expect("Couldn't create output directory.");
     }
 
-    if shader_dir.is_dir() {
-        let shader_dir = shader_dir.read_dir().unwrap();
-        //TODO: recursively check directories for shaders
-        for shader in shader_dir {
-            let shader_path = shader.unwrap().path();
-
-            let shader_type = {
-                match shader_path.extension() {
-                    Some(extension) if extension == "vert" => shaderc::ShaderKind::Vertex,
-                    Some(extension) if extension == "frag" => shaderc::ShaderKind::Fragment,
-                    _ => {
-                        panic!("Unknown shader type.");
-                    }
-                }
-            };
-
-            let source_name = shader_path.file_name().unwrap().to_str().unwrap();
-            let mut shader_file = File::open(shader_path.clone()).expect("Couldn't open file.");
-            let mut source = String::from("");
-
-            let _bytes = shader_file.read_to_string(&mut source).expect("Couldn't write file to string. Maybe not valid UTF-8?");
-
-            let source_name_stem = shader_path.file_stem().unwrap().to_str().unwrap();
-
-            let output_file_name = match shader_type {
-                shaderc::ShaderKind::Vertex => {
-                    OUTPUT_DIR.to_owned() + &source_name_stem.to_owned() + &OUTPUT_EXT.to_owned()
-                }
-                shaderc::ShaderKind::Fragment => {
-                    OUTPUT_DIR.to_owned() + &source_name_stem.to_owned() + &OUTPUT_EXT.to_owned()
-                }
-                _ => panic!("Unimplemented shader type."),
-            };
-
-            println!("Shader source name: {}", source_name);
-            let compiler_artifact = shader_compiler
-                .compile_into_spirv(
-                    &source,
-                    shader_type,
-                    source_name,
-                    "main",
-                    Some(&compiler_options),
-                )
-                .unwrap();
-
-            let mut binary_file = File::create(&output_file_name).unwrap();
-            binary_file.write_all(compiler_artifact.as_binary_u8()).expect("Couldn't write.");
-        }
-    } else {
+    let shader_dir = shader_dir.read_dir().unwrap();
+    if !shader_dir.is_dir() {
         panic!("The shaders directory does not exist.");
+    }
+
+    //TODO: recursively check directories for shaders
+    for shader in shader_dir {
+        let shader_path = shader.unwrap().path();
+
+        let shader_type = {
+            match shader_path.extension() {
+                Some(extension) if extension == "vert" => shaderc::ShaderKind::Vertex,
+                Some(extension) if extension == "frag" => shaderc::ShaderKind::Fragment,
+                _ => {
+                    panic!("Unknown shader type.");
+                }
+            }
+        };
+
+        let source_name = shader_path.file_name().unwrap().to_str().unwrap();
+        let mut shader_file = File::open(shader_path.clone()).expect("Couldn't open file.");
+        let mut source = String::from("");
+
+        let _bytes = shader_file.read_to_string(&mut source).expect("Couldn't write file to string. Maybe not valid UTF-8?");
+
+        let source_name_stem = shader_path.file_stem().unwrap().to_str().unwrap();
+
+        let output_file_name = match shader_type {
+            shaderc::ShaderKind::Vertex => {
+                OUTPUT_DIR.to_owned() + &source_name_stem.to_owned() + &OUTPUT_EXT.to_owned()
+            }
+            shaderc::ShaderKind::Fragment => {
+                OUTPUT_DIR.to_owned() + &source_name_stem.to_owned() + &OUTPUT_EXT.to_owned()
+            }
+            _ => panic!("Unimplemented shader type."),
+        };
+
+        println!("Shader source name: {}", source_name);
+        let compiler_artifact = shader_compiler
+            .compile_into_spirv(
+                &source,
+                shader_type,
+                source_name,
+                "main",
+                Some(&compiler_options),
+            )
+            .unwrap();
+
+        let mut binary_file = File::create(&output_file_name).unwrap();
+        binary_file.write_all(compiler_artifact.as_binary_u8()).expect("Couldn't write.");
     }
 
     match profile.as_str() {
